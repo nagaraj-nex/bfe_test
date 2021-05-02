ba2bd151e69914cd38f6d23d96a2a84608404530
Nagaraj <nravinuthala@gmail.com>
2021-05-02 23:12:40 +0530

    added host param to Session class

--- test_session.py
+++ test_session.py
@@ -2,6 +2,6 @@ import os
 os.environ['BFE_SSL_CERT'] = 'ubuntu2004.crt'
 
 from pybfe.client.session import Session
-bfe = Session('ubuntu2004')
+bfe = Session(host='ubuntu2004')
 network_list = bfe.list_networks()
 print(network_list)
